5749bf9f1202467e2eda2621a03627860ae34846
pitsanu <p.tongprasin@gmail.com>
2014-04-15 10:07:03 +0700

    Complete chapter 8

---
+++ SpriteKitPhysicsTest/SpriteKitPhysicsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitPhysicsTest
+//
+//  Created by Nu on 4/14/14.
+//  Copyright (c) 2014 The Big Round Mud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
